997a1aba0f0174040f1880a3021455709901606a
Will Darkins <willdarkins@gmail.com>
2021-10-22 16:55:50 -0700

    PORT and MySQL server connected to project

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+require('dotenv').config()
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
