941ac3833d9aacc528b1ca3317d55674907e467c
Will Darkins <willdarkins@gmail.com>
2021-10-23 12:39:06 -0700

    Sample data input to seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Roger", "Federer", 1),
+("Rafael", "Nadal", 2),
+("Novak", "Djokovic", 3),
+("Daniil", "Medvedev", 4),
+("Stefanos", "Tsitsipas", 5),
+("Alexander", "Zverev", 6),
+("Dominic", "Thiem", 7),
+("Gael", "Monfils", 8),
+("Grigor", "Dimitrov", 9),
+("John", "Isner", 10);
+
--- inquirer/index.js
+++ inquirer/index.js
@@ -71,7 +71,84 @@ const addRole = () => {
 }
 
 const addEmployee = () => {
-    console.log('This works!')
+        console.log(`
+        ==================
+           Add Employee
+        ==================
+        `);
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'employeefirstname',
+                message: 'What\'s your employee\'s first name?',
+                validate: nameanswer => {
+                    if (nameanswer) {
+                        return true;
+                    } else {
+                        console.log('\nAn employee name is required:')
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'employeelastname',
+                message: 'What\'s your employee\'s last name?',
+                validate: lastnameanswer => {
+                    if (lastnameanswer) {
+                        return true;
+                    } else {
+                        console.log('\nAn employee last name is required:')
+                        return false;
+                    }
+                }
+            },
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'roleselect',
+                    message: 'What is the employee\'s role?:',
+                    choices: ['Saleslead', 'Salesperson', 'Lead Engineer',
+                        'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
+                }
+            ])
+                .then(({ mainselect }) => {
+                    switch (mainselect) {
+                        case 'View All Departments':
+                            viewAllDepartments()
+                            break;
+                        case 'View All Roles':
+                            viewAllRoles()
+                            break;
+                        case 'View All Employees':
+                            viewAllEmployees()
+                            break;
+                        case 'Add a Department':
+                            addDepartment()
+                            break;
+                        case 'Add a Role':
+                            addRole()
+                            break;
+                        case 'Add an Employee':
+                            addEmployee()
+                            break;
+                        case 'Update an Employee Role':
+                            updateRole()
+                            break;
+                        default:
+                            ''
+                            break;
+                    }
+                })
+            
+        ])
+        /*Taking input from questions to build new Engineer object and push to global teammembers array.
+        Takes user to continuePrompt function to decide on new members, or to finish.*/
+            .then((input) => {
+                const employee = new Engineer(input.name, input.id, input.email, input.github);
+                teamMembers.push(employee);
+                continuePrompt();
+            })
 }
 
 const updateRole = () => {
