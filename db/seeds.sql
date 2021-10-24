8289c1375db27d046973226c583278eedd9ea31b
Will Darkins <willdarkins@gmail.com>
2021-10-24 15:01:53 -0700

    figured out that I need to add the acutal null value to data if it is infact null

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,14 +18,14 @@ VALUES
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-(1, 'Roger', 'Federer', 1),
-(2, 'Rafael', 'Nadal', 3),
-(3, 'Novak', 'Djokovic', 7),
-(4, 'Andy', 'Murray', 5),
+(1, 'Roger', 'Federer', 1, NULL),
+(2, 'Rafael', 'Nadal', 3, NULL),
+(3, 'Novak', 'Djokovic', 7, NULL),
+(4, 'Andy', 'Murray', 5, NULL),
 (5, 'Daniil', 'Medvedev', 8, 3),
 (6, 'Stefanos', 'Tsitsipas', 8, 3),
 (7, 'Alexander', 'Zverev', 4, 2),
-(8, 'Dominic', 'Thiem', 4, 2),
+(8, 'Dominic', 'Thiem', 4, 2,),
 (9, 'Gael', 'Monfils', 2, 1),
 (10, 'Grigor', 'Dimitrov', 2, 1),
 (11, 'John', 'Isner', 6, 4);
