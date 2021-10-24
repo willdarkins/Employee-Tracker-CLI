3f4996d960b2a628bf298240e9f96468653fc679
Will Darkins <willdarkins@gmail.com>
2021-10-24 15:31:43 -0700

    deleted repeated console log message about router

--- db/connection.js
+++ db/connection.js
@@ -6,9 +6,9 @@ const db = mysql.createConnection(
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        database: 'employees'
+        database: 'staff_db'
     },
-    console.log('Connected to the employees database.')
+    console.log('Connected to the staff_db database.')
 );
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3003;
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Use apiRoutes
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -20,7 +20,6 @@ app.use((req, res) => {
 // Start server after DB connection
 db.connect(err => {
   if (err) throw err;
-  console.log('Database connected.');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
