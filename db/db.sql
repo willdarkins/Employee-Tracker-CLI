e15403f681f0e0adc1ccb89a6252dbad5a1574b5
Will Darkins <willdarkins@gmail.com>
2021-10-28 23:40:12 -0700

    seedsdb notes entered

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,4 @@
+/*Following SQL statements delete exisiting(if) staff_db, create new database and direct MySQL to use said database*/
 DROP DATABASE IF EXISTS staff_db;
 CREATE DATABASE staff_db;
 USE staff_db;
