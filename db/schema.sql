1f52e25fdbba7a5c9d3dcb52b48e41580658da2d
Will Darkins <willdarkins@gmail.com>
2021-10-28 23:38:32 -0700

    schema notes written

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
+/*Deletes tables to create new data*/
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
-
+/*Department table*/
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
-
+/*Role table*/
 CREATE TABLE role (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id),
     PRIMARY KEY (id)
 );
-
+/*Employee table*/
 CREATE TABLE employee (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
